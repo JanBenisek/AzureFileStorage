69d370d30e56619725b4f0dfc7bfcd0780ab8d6e
JanBenisek <benisekj@gmail.com>
2020-10-18 11:49:08 +0200

    removed if name

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def upload_file():
             return redirect(request.url)
 
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+#if __name__ == "__main__":
+#    app.run(host='127.0.0.1', port=5000)

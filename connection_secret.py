eac2afa98116331beda814ff7aedcf8a09413f63
JanBenisek <benisekj@gmail.com>
2020-10-20 16:46:50 +0200

    conn secrt

---
+++ connection_secret.py
@@ -0,0 +1,2 @@
+conn_str = "DefaultEndpointsProtocol=https;AccountName=uploadtoolstorage;AccountKey=qFvP24zos3oIvf5XLZ8beLQ7m/ITPAwD0HpCAziwR/6m7B4bEm/P0Qoum57neDF0jgAd4ZB1XnWf5GH0ocsBGg==;EndpointSuffix=core.windows.net"
+secret_key = 'nemameamitnebudeme'

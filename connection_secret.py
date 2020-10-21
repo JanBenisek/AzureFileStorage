442ede14192cd013175ad1e4d449834ccacf82f0
Jan Benisek <benisekj@gmail.com>
2020-10-21 19:47:34 +0200

    removed connection

--- connection_secret.py
+++
@@ -1,2 +0,0 @@
-conn_str = "DefaultEndpointsProtocol=https;AccountName=uploadtoolstorage;AccountKey=l1dS+pvCNDnmO1wfK3jGq99JPj3O0dM6dwTZGnF4vhvPL1j6GoLTWOBtRdXHSLrCfsRPXOhPkS6/cTyKnnNdRg==;EndpointSuffix=core.windows.net"
-secret_key = 'nemameamitnebudeme'
